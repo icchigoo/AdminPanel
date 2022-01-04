6d0819ce6e2d9ff0bf4325f42f53cc120e2e452f
Aawash cheetry <79775763+icchigoo@users.noreply.github.com>
2022-01-04 01:41:09 -0800

    data base connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:admin_panel/screens/admin.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Admin(),
-  ));
+void main() async {
+  // These two lines
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  //
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Admin(),
+    );
+  }
 }
